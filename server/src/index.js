a0cfa904b4c106b7cf8013f49c6b3aa30a73985a
Ken Charette <charettekenneth@gmail.com>
2020-08-05 20:35:57 -0400

    Connection made to mongodb and setup env variables using dotenv

--- server/src/index.js
+++ server/src/index.js
@@ -1,17 +1,28 @@
 const express = require('express');
-
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-const port = process.env.PORT || 1337;
+const postsRoute = require('./routes/posts');
+
+mongoose.connect(process.env.MONGO_URI, 
+    { 
+        useNewUrlParser: true, 
+        useUnifiedTopology: true,
+    },
+    () => {
+        console.log('Connected to the DB')
+    }
+);
+
+app.use('/posts', postsRoute);
 
 app.get('/', (req, res) => {
     res.send('Our first route');
 })
 
-app.get('/posts', (req, res) => {
-    res.send('Welcome to the posts route');
-})
 
+const port = process.env.PORT || 1337;
 app.listen(port, () => {
     console.log(`Listening on port http://localhost:${port}`);
 })
